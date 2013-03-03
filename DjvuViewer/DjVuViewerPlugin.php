5df129634f74ed7714f633960760791124efb6a9
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2013-03-03 18:38:53 +0100

    remove unsues hook & unsuded options

--- DjvuViewer/DjVuViewerPlugin.php
+++ DjvuViewer/DjVuViewerPlugin.php
@@ -19,7 +19,6 @@ class DjvuViewerPlugin extends Omeka_Plugin_Abstract
         'config_form',
         'config',
         'define_routes',
-        'public_append_to_items_show',
     );
 
     const DEFAULT_VIEWER_EMBED = 1;
@@ -30,9 +29,6 @@ class DjvuViewerPlugin extends Omeka_Plugin_Abstract
 
     public function hookInstall()
     {
-        set_option('djvuviewer_embed_admin', DjvuViewerPlugin::DEFAULT_VIEWER_EMBED);
-        set_option('djvuviewer_width_admin', DjvuViewerPlugin::DEFAULT_VIEWER_WIDTH);
-        set_option('djvuviewer_height_admin', DjvuViewerPlugin::DEFAULT_VIEWER_HEIGHT);
         set_option('djvuviewer_embed_public', DjvuViewerPlugin::DEFAULT_VIEWER_EMBED);
         set_option('djvuviewer_width_public', DjvuViewerPlugin::DEFAULT_VIEWER_WIDTH);
         set_option('djvuviewer_height_public', DjvuViewerPlugin::DEFAULT_VIEWER_HEIGHT);
@@ -51,15 +47,11 @@ class DjvuViewerPlugin extends Omeka_Plugin_Abstract
 
     public function hookConfig($post)
     {
-        if (!is_numeric($post['djvuviewer_width_admin']) ||
-            !is_numeric($post['djvuviewer_height_admin']) ||
-            !is_numeric($post['djvuviewer_width_public']) ||
+        if (!is_numeric($post['djvuviewer_width_public']) ||
             !is_numeric($post['djvuviewer_height_public'])) {
             throw new Exception('The width and height must be numeric.');
         }
-        set_option('djvuviewer_embed_admin', (int) (boolean) $post['djvuviewer_embed_admin']);
-        set_option('djvuviewer_width_admin', $post['djvuviewer_width_admin']);
-        set_option('djvuviewer_height_admin', $post['djvuviewer_height_admin']);
+
         set_option('djvuviewer_embed_public', (int) (boolean) $post['djvuviewer_embed_public']);
         set_option('djvuviewer_width_public', $post['djvuviewer_width_public']);
         set_option('djvuviewer_height_public', $post['djvuviewer_height_public']);
@@ -81,54 +73,46 @@ class DjvuViewerPlugin extends Omeka_Plugin_Abstract
         );
     }
 
-    public static function hookPublicAppendToItemsShow( $currentFile = "" )
+    public function djvuviewer_embed ( $Files = "" )
     {
-        // Embed viewer only if configured to do so.
-        if ((is_admin_theme() && !get_option('djvuviewer_embed_admin')) ||
-            (!is_admin_theme() && !get_option('djvuviewer_embed_public'))) {
-            return;
+
+        if (!is_array($Files)) {
+            $Files = array($Files);
         }
-        $docsViewer = new DjvuViewerPlugin;
-        $docsViewer->djvuviewer_embed( $currentFile );
-    }
 
-    public function djvuviewer_embed ( $currentFile = "" )
-    {
-
-        foreach (__v()->item->Files as $file) {
-            if($file->id == $currentFile) {
-                $extension = pathinfo($file->archive_filename, PATHINFO_EXTENSION);
-                if (!in_array($extension, $this->_supportedFiles)) {
-                    continue;
-                }
-
-                ?>
-                <div>
-                    <script src="<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR; ?>/deployJava.js"></script>
-                    <script>
-                    var attributes = {
-                                      codebase:'<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>',
-                                      code:'com.lizardtech.djview.Applet.class',
-                                      archive:'<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>/javadjvu.jar',
-                                      width:<?php echo is_admin_theme() ? get_option('djvuviewer_width_admin') : get_option('djvuviewer_width_public'); ?>,
-                                      height:<?php echo is_admin_theme() ? get_option('djvuviewer_height_admin') : get_option('djvuviewer_height_public'); ?>
-                    } ;
-                    var parameters = {
-                            cache_archive:"<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>/javadjvu.jar",
-                            data:"<?php echo $this->_getUrl($file); ?>"
-                    } ;
-                    if (deployJava.versionCheck('1.6')) {
-                        deployJava.runApplet(attributes, parameters, '1.6');
-                    } else {
-                        document.write('<div style="margin: 100px auto; width: 400px; font-size: 1.5em">');
-                        document.write('Java plugin is necessary to view this page. <a href="http://java.com" target="_blank">Click here</a> to install.');
-                        document.write('</div>');
-                    }
-                    </script>
-                </div>
-
-                <?php
+        foreach ($Files as $file) {
+            $extension = pathinfo($file->archive_filename, PATHINFO_EXTENSION);
+            if (!in_array($extension, $this->_supportedFiles)) {
+                continue;
             }
+
+            echo $file;
+            ?>
+            <div>
+                <script src="<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR; ?>/deployJava.js"></script>
+                <script>
+                var attributes = {
+                                  codebase:'<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>',
+                                  code:'com.lizardtech.djview.Applet.class',
+                                  archive:'<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>/javadjvu.jar',
+                                  width:<?php echo is_admin_theme() ? get_option('djvuviewer_width_admin') : get_option('djvuviewer_width_public'); ?>,
+                                  height:<?php echo is_admin_theme() ? get_option('djvuviewer_height_admin') : get_option('djvuviewer_height_public'); ?>
+                } ;
+                var parameters = {
+                        cache_archive:"<?php echo WEB_PLUGIN . DIRECTORY_SEPARATOR . 'DjvuViewer' . DIRECTORY_SEPARATOR . 'applet' ?>/javadjvu.jar",
+                        data:"<?php echo $this->_getUrl($file); ?>"
+                } ;
+                if (deployJava.versionCheck('1.6')) {
+                    deployJava.runApplet(attributes, parameters, '1.6');
+                } else {
+                    document.write('<div style="margin: 100px auto; width: 400px; font-size: 1.5em">');
+                    document.write('Java plugin is necessary to view this page. <a href="http://java.com" target="_blank">Click here</a> to install.');
+                    document.write('</div>');
+                }
+                </script>
+            </div>
+
+            <?php
         }
     }
 
